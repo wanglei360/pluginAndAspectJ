558335acb0cbffae9b5b94235c69b3b7874c667a
leiwu <wang_lei2005@126.com>
2022-03-22 14:37:59 +0800

    gradle之后插件制作与使用更改在说明中添加

---
+++ PluginDemoCode/KotlinAspect.kt
@@ -0,0 +1,24 @@
+package com.jni.myapplication
+
+
+import android.util.Log
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.annotation.After
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+
+/**
+ * java 和 kotlin都能用
+ */
+@Aspect
+class KotlinAspect {
+    @Before("execution(* btnClik(..))")
+    fun before(joinPoint: JoinPoint) {
+        Log.d("asdfasdf", "之前执行")
+    }
+
+    @After("execution(* btnClik(..))")
+    fun after(joinPoint: JoinPoint) {
+        Log.d("asdfasdf", "之后执行")
+    }
+}
